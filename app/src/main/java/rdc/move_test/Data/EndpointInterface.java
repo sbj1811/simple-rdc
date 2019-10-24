241d637b5df63b29d293115bb3cc099ac2ec74a4
sbj1811 <sbj1811@.github.com>
2019-10-24 11:57:15 -0700

    Fixed Endpoint

--- app/src/main/java/rdc/move_test/Data/EndpointInterface.java
+++ app/src/main/java/rdc/move_test/Data/EndpointInterface.java
@@ -9,6 +9,6 @@ import retrofit2.http.Query;
 public interface EndpointInterface {
     String API_ENDPOINT =  "https://mapi-ng.rdc.moveaws.com";
 
-    @GET("/api/v1/properties?limit=5&city=Napa&state_code=CA&sort=relevance&client_id=rdc_mobile_native")
+    @GET("/api/v1/properties")
     Call<Properties> getProperties(@Query("limit") String limit, @Query("city") String city, @Query("state_code") String state_code, @Query("sort") String sort, @Query("client_id") String client_id);
 }
